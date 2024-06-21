8ce46129213e65482334c6e73e14ef8e300f0881
RISHIK396 <163277910+RISHIK396@users.noreply.github.com>
2024-06-21 13:12:49 +0530

    Add files via upload

---
+++ javascript/tut2.js
@@ -0,0 +1,5 @@
+console.log("hello world welcome this is tut 2 of javascript")
+let a=67
+console.log(a)
+a="harry"
+console.log(a)
---
+++ javascript/tut5.js
@@ -0,0 +1,29 @@
+// chapter 1 practise set
+// Q1
+let a="HARRY";
+console.log(a+12);
+
+// Q2
+console.log(typeof(a+12));
+
+// Q3
+
+const b={
+    name:"harry",
+    section : 1,
+    isprincipal:false
+}
+// Q.4
+
+b["friend"]="shubam";
+b["name"]="rishik";
+console.log(b);
+
+
+// Q5
+const dict={
+    appretiate:"recognize the full worth",
+    yakka:"work,especially hard work"
+}
+
+console.log(dict['yakka'])

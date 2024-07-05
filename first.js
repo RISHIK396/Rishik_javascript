e973e238bbb81cb7dd23f45aae14f34d5ab356d2
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-07-05 22:38:24 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,32 @@
+
+// level 1 in javascript here we will see about variables concept 
+
+
+// we can use consol.log to print the value in console block of javascript
+console.log("hello world");
+
+//below you can see variables which are containers which are used to store values in javascript 
+let a =10;
+let b= 20;
+
+// printing the value of a and b variables
+console.log("printing the value of a and b: ",a,"and",b);
+
+// now we will see some valid and invalid variable names in javascript
+
+// this is invalid
+// let @tony_stark="hello";
+
+// this is also iinvalid
+// let $name=
+
+// THIS IS A VALID VARIABLE NAME
+let name123_$="TONY STARK";
+
+
+// BOTH THE BELOW METIONED VARIABLES ARE DIFFERENT
+let apple=20;
+let Apple=25;
+
+console.log(apple);
+console.log(Apple);

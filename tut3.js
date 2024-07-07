d782b261186553c298a16af329e7960416787b9e
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-07-07 12:05:53 +0530

    Add files via upload

---
+++ tut3.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+
+// conditional statements in javascrpit
+
+// if statement
+
+let mode=prompt("Please enter the mode you want in the backgroud: ");
+let color;
+
+
+if(mode==="dark mode"){
+    color="black";
+}
+
+// if-else statement
+
+else{
+    color="white";
+}
+
+console.log(color);
+
+
+
+
+

97eedc9c9a93a1215aee27141e0324362b0ab0db
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-07-08 12:04:40 +0530

    Add files via upload

---
+++ tut4.js
@@ -0,0 +1,10 @@
+// checking whether entered number is even or odd 
+let num=prompt("please enter the number to be checked :")
+
+if(num%2===0){
+    alert("you have entered an even number");
+}
+
+else{
+    alert("no you have entered an odd number")
+}

2d0b832ddba74de1fbbeedd19a99806001fdcffb
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-07-20 17:45:02 +0530

    Add files via upload

---
+++ dom3.js
@@ -0,0 +1,15 @@
+let div=document.querySelector("div");
+// console.log(div);
+// console.dir(div);
+let class1=div.getAttribute("class");
+
+// will print the value which is stred in the class attribute of the div tag
+console.log(class1);
+
+// first ascessing the paragraph tag with the help of the document object
+let par1=document.querySelector("p");
+
+// then with the help of getAttribute function printing the value stored in the reqired attributr
+let name1=par1.getAttribute("name");
+console.log(name1);
+

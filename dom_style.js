0f8c8fc58315b8e10a619ce12cfa6d2a801295eb
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-07-23 22:50:57 +0530

    Add files via upload

---
+++ dom_style.js
@@ -0,0 +1,12 @@
+let head1=document.querySelector("h1");
+
+head1.style.backgroundColor="green";
+
+// changin th eback ground color of the second heading with the help of javascript only
+let head2=document.querySelector("h2");
+
+head2.style.backgroundColor ="blue";
+
+head2.style.color="white";
+head2.style.border="3px solid black";
+head2.style.borderRadius="10px";

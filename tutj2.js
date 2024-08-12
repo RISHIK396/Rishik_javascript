bde45153e705babbff49901adb6d557f16699417
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-08-12 13:50:13 +0530

    Add files via upload

---
+++ tutj2.js
@@ -0,0 +1,33 @@
+// this is a single line comment
+/*this is a multiline comment*/
+
+// operators in javasript used to perform some operations on the data
+
+// arithematic operators
+
+let a=12;
+let b=20;
+// plus operator
+console.log(a+b);
+
+// multiply operator
+console.log(a*b);
+
+// division operator
+console.log(a/b);
+
+// modulous operator
+console.log(b%a);
+
+// exponentiation opertator
+console.log(a**2);
+
+// unary operators
+console.log("the value of a is: ",a);
+console.log("the value of b is: ",b);
+console.log("the value of a after post increment is: ",a++);
+console.log("value of a: ",a);
+console.log("the value of b after preincrement is: ",++b);
+
+
+

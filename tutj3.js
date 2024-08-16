9e44304f77adb0b0377e6103a9657bdea04b8ca2
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-08-16 23:32:33 +0530

    Add files via upload

---
+++ tutj3.js
@@ -0,0 +1,29 @@
+// comparision operators in javascript which is used to comapare two value in javascript
+
+let a=20;
+let b=30;
+let c = 30;
+
+console.log("the value of a > b is: ",a>b);
+console.log("the value of a < b is: ",a<b);
+console.log("the value of b>=c and c<=b are",b>=c,b<=c);
+
+// this is equals to and type comparision
+console.log("2===2",2==="2");
+
+console.log("2!= \"2\" value is: ",2!="2");
+
+// not equal to and data type
+console.log(2!=="2");
+
+// logical opertaors in javascript
+
+console.log(1||1);
+console.log(1||0);
+
+// And logical operator
+console.log(0 && 1);
+
+//not logical operator
+
+console.log(!0);

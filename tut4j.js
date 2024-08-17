3b8550ba5b76b32970ba353a406e6d5ff2048c59
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2024-08-17 23:46:47 +0530

    Add files via upload

---
+++ tut4j.js
@@ -0,0 +1,25 @@
+// In this we will study about conditional statements
+
+// checking whether a number is even or odd
+
+let num1=10;
+
+if(num1%2==0){
+    console.log("number is an even number ")
+}
+
+else{
+    console.log("number is not an even number ")
+}
+
+// person adult or not
+
+let age=20;
+if(age>18){
+    console.log("the person is adult");
+
+}
+
+else{
+    console.log("person is not adult");
+}

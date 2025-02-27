2380ac6f89fec87aa7ba63f9eab4d765e29cb972
RISHIK CHAUDHARY <163277910+RISHIK396@users.noreply.github.com>
2025-02-27 23:25:41 +0530

    Add files via upload

---
+++ object1.js
@@ -0,0 +1,9 @@
+// this is the first time i will use ibjects in javascript
+
+const student = {
+    name : "Rishik Chaudhary",
+    marks : 94,
+};
+
+console.log(student.name);
+console.log(student.marks);
